f8622b9cd7c4c8355db3687a9d05b834d3db9216
Jeremy Gale <jeremy.gale@atco.com>
2020-09-08 11:27:40 -0600

    Initial Commit

---
+++ TabBarAccessibility14/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabBarAccessibility14
+//
+//  Created by Jeremy Gale on 2020-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
